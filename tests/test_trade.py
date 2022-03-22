4c9f97b4be131f79c9d6725a0be09b1f1d432e53
Kyle _Jorgensen <kyk_n_wyng@yahoo.com>
2022-03-21 19:58:52 -0500

    First Dev commit

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -5,7 +5,7 @@ THIS CODE IS PROVIDED AS IS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND
 This file is part of the Open Source Library (www.github.com/KJJorgensen)
 --------------------------------------------------------------------------------------------------------------------"""
 
-from fastapilib import trade
+from ..fastapilib import trade
 
 data = {'ticker': 'RCL', 'close': 68.17, 'quantity': 2, 'dte': 30, 'callput': 'CALL', 'instruction': 'BUY_TO_OPEN', 'passphrase': 'SScamaro98'}
 
